f1486fd1782d9957ebdff9e27f0b8f5e7d683f18
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-01-16 12:32:47 +0000

    xbvb

--- header-and-footer/calling.js
+++ header-and-footer/calling.js
@@ -11,6 +11,39 @@ fetch('./header-and-footer/footer.html')
     document.querySelector("footer").innerHTML = data;
 });
 
-document.getElementById("up").addEventListener("click", up)
+// document.getElementById("up").addEventListener("click", up)
 
-function up() {}
+// function up() {}
+
+let btn1 = document.getElementById("myFunction");
+let btn2 = document.getElementById("myFunction1");
+
+let x = document.getElementById("sign-up");
+let y = document.getElementById("sign-in");
+
+
+
+
+btn2.addEventListener("click", form1)
+function form1(){
+    if((y.style.display = "block")) {
+        x.style.display = 'none';
+    }
+    else {
+        y.style.display = 'block';
+    }
+}
+
+
+btn1.addEventListener("onclick", form2())
+function form2(){
+    console.log("hi");
+    if((x.style.display = "block")) {
+        y.style.display = 'none';
+    }
+    else {
+        x.style.display = 'block';
+    }
+}
+
+
