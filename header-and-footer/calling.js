ae05a65d92c43949b3919cd5fc5ed71a8c7893af
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-01-16 10:20:37 +0000

    sign in

--- header-and-footer/calling.js
+++ header-and-footer/calling.js
@@ -10,3 +10,21 @@ fetch('./header-and-footer/footer.html')
 .then((data) => {
     document.querySelector("footer").innerHTML = data;
 });
+
+document.getElementById("up").addEventListener("click", up)
+
+function up() {
+    fetch('./header-and-footer/up.html')
+.then((response) => response.text())
+.then((data) => {
+    document.querySelector("form1").innerHTML = data;
+})}
+
+// document.getElementById("in").addEventListener("click", in)
+
+// function in() {
+//     fetch('./header-and-footer/in.html')
+// .then((response) => response.text())
+// .then((data) => {
+//     document.querySelector("form2").innerHTML = data;
+// })}
