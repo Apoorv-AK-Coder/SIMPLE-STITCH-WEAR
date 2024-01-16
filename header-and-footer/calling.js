6ce937158055f1baeb6923258e77355d46f9a115
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-01-16 11:00:40 +0000

    sdfhf

--- header-and-footer/calling.js
+++ header-and-footer/calling.js
@@ -11,17 +11,6 @@ fetch('./header-and-footer/footer.html')
     document.querySelector("footer").innerHTML = data;
 });
 
-// document.getElementById("up").addEventListener("click", up)
+document.getElementById("up").addEventListener("click", up)
 
-// function up() {
-    
-// })}
-
-// document.getElementById("in").addEventListener("click", in)
-
-// function in() {
-//     fetch('./header-and-footer/in.html')
-// .then((response) => response.text())
-// .then((data) => {
-//     document.querySelector("form2").innerHTML = data;
-// })}
+function up() {}
