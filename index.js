1d86cae34144c7c5ba1fc2727c8a60c9e8b5729a
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-01-18 06:31:34 +0000

    xcvn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { MongoClient } = require("mongodb");
+let mongodb;
+const { RemoteMongoClient } = mongodb;
 
 const username = encodeURIComponent("simplestitchwear");
 const password = encodeURIComponent("Himani%40%23123");
-const cluster = "<clusterName>";
-const authSource = "<authSource>";
-const authMechanism = "<authMechanism>";
+const cluster = "sign-up.kammi.mongodb.net";
+const authSource = "atlasAdmin";
+const authMechanism = "SCRAM-SHA-256";
 
 let uri =
   `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
@@ -15,12 +16,13 @@ async function run() {
   try {
     await client.connect();
 
-    const database = client.db("<dbName>");
-    const ratings = database.collection("<collName>");
+    const database = client.db("sample_mflix");
+    const ratings = database.collection("movies");
 
     const cursor = ratings.find();
 
     await cursor.forEach(doc => console.dir(doc));
+    console.log(database);
   } finally {
     await client.close();
   }
