376e47fb602c5ee9af503a17ec4b8e4b4750b640
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-02-03 07:30:03 +0000

    dxhg

--- sign-up.js
+++ sign-up.js
@@ -1,48 +1,34 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD5D005fXr5ghftEkda83Hfur7YP682WkA",
-  authDomain: "simple-stitch-wear-412007.firebaseapp.com",
-  databaseURL: "https://simple-stitch-wear-412007-default-rtdb.firebaseio.com",
-  projectId: "simple-stitch-wear-412007",
-  storageBucket: "simple-stitch-wear-412007.appspot.com",
-  messagingSenderId: "623368346702",
-  appId: "1:623368346702:web:3fb9b9f1df34d35a27371f",
-  measurementId: "G-QH3385Y2X6"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-  // Reference to the Firebase database
-  var database = firebase.database();
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAV348JSscviViD2GQTfQ-Ej5ywF0zEJBk",
+    authDomain: "simple-stitch-wear-49549.firebaseapp.com",
+    databaseURL: "https://simple-stitch-wear-49549-default-rtdb.firebaseio.com",
+    projectId: "simple-stitch-wear-49549",
+    storageBucket: "simple-stitch-wear-49549.appspot.com",
+    messagingSenderId: "20554738672",
+    appId: "1:20554738672:web:9d3cb92e1fde0039904b35",
+    measurementId: "G-N2235GQ1G6"
+  };
 
 
-function submitForm() {
-    var fullName = document.getElementById('uname').value;
-    var userId = document.getElementById('uid').value;
-    var email = document.getElementById('mail').value;
-    var phoneNumber = document.getElementById('number').value;
-    var password = document.getElementById('password').value;
-  
-    // Push data to Firebase
-    database.ref('users').push({
-      full_name: fullName,
-      user_id: userId,
-      email: email,
-      phone_number: phoneNumber,
-      password: password
-    });
-  
-    alert('Data submitted successfully!');
-  }
-  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
+  const data = {
+    fullName: "John Doe",
+    userId: "johndoe123",
+    email: "john@example.com",
+ };
+ 
+ // Push data to Firebase
+ const newRef = database.ref("Sign-Up").push(data);
+ console.log("Data added with key:", newRef.key);
